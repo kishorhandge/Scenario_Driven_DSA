8fc125c2c4c9025216b56237379b9d01e699694b
Kishor Suryabhan Handge <kishorhandge29@gmail.com>
2026-06-09 22:20:19 +0530

    Add files via upload

---
+++ HighestOccNo.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+class HighestOccNo
+{
+    public static void main(String A[]) 
+    {
+        int iNo = 0,iCnt = 0,iCount = 0;
+
+        Scanner sobj = new Scanner(System.in);
+
+        boolean bRet[] = new boolean[false];
+
+        System.out.println("Enter the Number");
+        iNo = sobj.nextInt();
+
+        int Arr[] = new int[iNo];
+
+        for(iCnt = 0;iCnt <iNo;iCnt++)
+        {
+            Arr[iCnt] = sobj.nextInt();
+        }
+
+        for(iCnt = 0;iCnt <iNo;iCnt++)
+        {
+            Arr[iCnt] = sobj.nextInt();
+        }
+
+
+
+
+
+
+    }
+}
